5f42fdd983e80a8c44e708b1324479fb04977584
nic-ball <nicholasball147@gmail.com>
2025-12-05 19:04:00 +0000

    Add auto-creation of superuser using ENV

---
+++ create_superuser.py
@@ -0,0 +1,19 @@
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setDefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+User = get_user_model()
+
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME','admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'thisIsNotTheWay69!')
+
+if not User.objects.filter(username=username).exists():
+    print(F"Creating superuser: {username}")
+    User.objects.create_superuser(username, email, password)
+    print("SUperuser created successfully.")
+else:
+    print(F"Superuser {username} already exists.")
